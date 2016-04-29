597fd76a534e0edb63188d0a776f36b98177999c
刘鑫伟 <liuxinwei1995@gmail.com>
2016-04-29 09:59:06 +0800

    The Student and Score Class OK.

---
+++ ScoreManager/src/com/scoreManager/database/Score.java
@@ -0,0 +1,93 @@
+package com.scoreManager.database;
+
+/**
+ * Score.
+ * 
+ * @author 刘鑫伟
+ *
+ */
+public class Score {
+
+	/**
+	 * math.
+	 */
+	private int math;
+
+	/**
+	 * Chinese.
+	 */
+	private int chinese;
+
+	/**
+	 * English.
+	 */
+	private int english;
+
+	/**
+	 * @return the math
+	 */
+	public int getMath() {
+		return math;
+	}
+
+	/**
+	 * @param math
+	 *            the math to set
+	 */
+	public void setMath(int math) {
+		this.math = math;
+	}
+
+	/**
+	 * @return the Chinese
+	 */
+	public int getChinese() {
+		return chinese;
+	}
+
+	/**
+	 * @param chinese
+	 *            the Chinese to set
+	 */
+	public void setChinese(int chinese) {
+		this.chinese = chinese;
+	}
+
+	/**
+	 * @return the English
+	 */
+	public int getEnglish() {
+		return english;
+	}
+
+	/**
+	 * @param english
+	 *            the English to set
+	 */
+	public void setEnglish(int english) {
+		this.english = english;
+	}
+
+	/**
+	 * Default constructor.
+	 */
+	public Score() {
+	}
+
+	/**
+	 * constructor using fields.
+	 * 
+	 * @param math
+	 *            Math.
+	 * @param chinese
+	 *            Chinese.
+	 * @param english
+	 *            English.
+	 */
+	public Score(int math, int chinese, int english) {
+		super();
+		this.math = math;
+		this.chinese = chinese;
+		this.english = english;
+	}
+}
--- ScoreManager/src/com/scoreManager/database/Student.java
+++ ScoreManager/src/com/scoreManager/database/Student.java
@@ -1,5 +1,90 @@
 package com.scoreManager.database;
 
+/**
+ * Student.
+ * 
+ * @author 刘鑫伟
+ *
+ */
 public class Student {
 
+	/**
+	 * id.
+	 */
+	private String id;
+
+	/**
+	 * name.
+	 */
+	private String name;
+
+	/**
+	 * score.
+	 */
+	private Score score;
+
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the score
+	 */
+	public Score getScore() {
+		return score;
+	}
+
+	/**
+	 * @param score
+	 *            the score to set
+	 */
+	public void setScore(Score score) {
+		this.score = score;
+	}
+
+	/**
+	 * Default constructor.
+	 */
+	public Student() {
+	}
+
+	/**
+	 * Constructor using fields.
+	 * 
+	 * @param id
+	 * @param name
+	 * @param score
+	 */
+	public Student(String id, String name, Score score) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.score = score;
+	}
 }
